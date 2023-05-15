8da5e0f99e5f45277a55309509efbaedd3a757e7
Bolesław Muzalewski <109333068+muzabol2@users.noreply.github.com>
2023-05-15 05:12:53 +0200

    Update Modify-PowerNSleep.ps1

--- Modify-PowerNSleep.ps1
+++ Modify-PowerNSleep.ps1
@@ -1,5 +1,12 @@
-# Power & Sleep
+## Power & Sleep
+
+# dc - on battery
+# ac - plugged in
+
+# Turn off the dispaly: 
 Powercfg /Change monitor-timeout-dc 5
 Powercfg /Change monitor-timeout-ac 10
+
+# Put the computer to sleep:
 Powercfg /Change standby-timeout-dc 15
-Powercfg /Change standby-timeout-ac 30
+Powercfg /Change standby-timeout-ac 360
